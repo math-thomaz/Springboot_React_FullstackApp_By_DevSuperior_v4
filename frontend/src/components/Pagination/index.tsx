5755dfd9f5177f42efe18a4cb0c8527f13163b0b
Matheus Thomaz <matheustthomaz@gmail.com>
2021-09-15 22:50:27 -0300

    Fixed pagination alignment

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -5,24 +5,24 @@ type Props = {
   onPageChange: Function;
 }
 
-function Pagination({ page, onPageChange } : Props) {
-  
+function Pagination({ page, onPageChange }: Props) {
+
   return (
-    <div className="row d-flex justify-content-center">
-    <nav>
+    <div className="d-flex justify-content-center">
+      <nav>
         <ul className="pagination">
-            <li className={`page-item ${page.first ? 'disabled' : ''}`}>
-                <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
-            </li>
-            <li className="page-item disabled">
-                <span className="page-link">{page.number + 1}</span>
-            </li>
-            <li className={`page-item ${page.last ? 'disabled' : ''}`}>
-                <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
-            </li>
+          <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+          </li>
+          <li className="page-item disabled">
+            <span className="page-link">{page.number + 1}</span>
+          </li>
+          <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
+          </li>
         </ul>
-    </nav>
-</div>
+      </nav>
+    </div>
   )
 }
 
